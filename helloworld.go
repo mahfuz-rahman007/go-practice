1b94f59a75ad5d2ac8c959b4c4e9361e7a2a7eb0
mahfuz-rahman007 <mahfujkhan017@gmail.com>
2024-04-01 12:01:24 +0600

    initial commit

---
+++ helloworld.go
@@ -0,0 +1,19 @@
+package main
+import ("fmt")
+
+
+func main(){
+
+  var (
+    a int = 434
+    c = "hello"
+    d float32
+  )
+
+
+  fmt.Println(a)
+  // fmt.Println(b)
+  fmt.Println(c)
+  fmt.Println(d)
+
+}
