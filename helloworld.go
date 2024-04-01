d9c39df785d4d7a38dff8590ec4bbdc4d7a4b599
mahfuz-rahman007 <mahfujkhan017@gmail.com>
2024-04-01 21:30:24 +0600

    iterate over map

--- helloworld.go
+++ helloworld.go
@@ -4,39 +4,21 @@ import (
 	"fmt"
 )
 
-type Person struct {
-  name string
-  age int
-  job string
-  salary int
-}
 
 func main() {
-  var pers1 Person
-  var pers2 Person
+  a := map[string]int{"one": 1, "two": 2, "three": 3, "four": 4}
 
-  // Pers1 specification
-  pers1.name = "Hege"
-  pers1.age = 45
-  pers1.job = "Teacher"
-  pers1.salary = 6000
+  var b []string             // defining the order
+  b = append(b, "one", "two", "three", "four")
 
-  // Pers2 specification
-  pers2.name = "Cecilie"
-  pers2.age = 24
-  pers2.job = "Marketing"
-  pers2.salary = 4500
+  for k, v := range a {        // loop with no order
+    fmt.Printf("%v : %v, ", k, v)
+  }
 
+  fmt.Println()
 
-  printPerson(pers1);
+  for _, element := range b {  // loop with the defined order
+    fmt.Printf("%v : %v, ", element, a[element])
+  }
 
-  printPerson(pers2);
-
-}
-
-func printPerson(person Person) {
-  fmt.Println("Name: ", person.name)
-  fmt.Println("Age: ", person.age)
-  fmt.Println("Job: ", person.job)
-  fmt.Println("Salary: ", person.salary)
 }
