177fad4b3d5daf8b35d4def80ebf1327a5faf9df
mahfuz-rahman007 <mahfujkhan017@gmail.com>
2024-04-01 21:09:37 +0600

    omit a return value if not used

--- helloworld.go
+++ helloworld.go
@@ -5,12 +5,12 @@ import (
 )
 
 func myFunction(x int, y string) (result int, txt1 string) {
-   result = x + x
-   txt1 = y + " World!"
-  return result, txt1
+  result = x + x
+  txt1 = y + " World!"
+  return
 }
 
 func main() {
-  a, b := myFunction(5, "Hello")
-  fmt.Println(a, b)
+   _, b := myFunction(5, "Hello")
+  fmt.Println(b)
 }
