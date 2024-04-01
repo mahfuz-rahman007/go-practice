e4d4b1194b597de8836b0adc376215df444454f2
mahfuz-rahman007 <mahfujkhan017@gmail.com>
2024-04-01 21:18:16 +0600

    go struct -> like object

--- helloworld.go
+++ helloworld.go
@@ -4,13 +4,38 @@ import (
 	"fmt"
 )
 
-func myFunction(x int, y string) (result int, txt1 string) {
-  result = x + x
-  txt1 = y + " World!"
-  return
+type Person struct {
+  name string
+  age int
+  job string
+  salary int
 }
 
 func main() {
-   _, b := myFunction(5, "Hello")
-  fmt.Println(b)
+  var pers1 Person
+  var pers2 Person
+
+  // Pers1 specification
+  pers1.name = "Hege"
+  pers1.age = 45
+  pers1.job = "Teacher"
+  pers1.salary = 6000
+
+  // Pers2 specification
+  pers2.name = "Cecilie"
+  pers2.age = 24
+  pers2.job = "Marketing"
+  pers2.salary = 4500
+
+  // Access and print Pers1 info
+  fmt.Println("Name: ", pers1.name)
+  fmt.Println("Age: ", pers1.age)
+  fmt.Println("Job: ", pers1.job)
+  fmt.Println("Salary: ", pers1.salary)
+
+  // Access and print Pers2 info
+  fmt.Println("Name: ", pers2.name)
+  fmt.Println("Age: ", pers2.age)
+  fmt.Println("Job: ", pers2.job)
+  fmt.Println("Salary: ", pers2.salary)
 }
